ad26b925a87c1f3836d1ced22089660d0f18a2b5
devinarena <d3v1n302418@gmail.com>
2022-07-04 14:00:34 -0400

    added compiling test cases and running individual cases

--- tests/tests.py
+++ tests/tests.py
@@ -11,6 +11,7 @@ import glob
 import os
 import subprocess
 
+
 ##
 # Runs a specific test case, comparing the output of the palladium file to the contents of the expected result file.
 #
@@ -18,7 +19,10 @@ import subprocess
 #
 def run_test(case):
     # Run the test case.
-    output = subprocess.check_output([os.path.join("..", "palladium.exe"), os.path.join("cases", case + ".pd")])
+    output = subprocess.check_output([
+        os.path.join("..", "palladium.exe"),
+        os.path.join("cases", case + ".pd")
+    ])
     # Compare the output to the expected result.
     expected = open(os.path.join("cases", case + ".expected.txt"), "r")
     passed = False
@@ -30,6 +34,7 @@ def run_test(case):
     expected.close()
 
     return passed
+
 
 ##
 # Uses glob to find all .pd test cases in the cases/ directory.
@@ -45,9 +50,29 @@ def run_all_tests():
             passed += 1
         else:
             failed += 1
-    
+
     print(f"\nPassed: {passed}")
     print(f"Failed: {failed}")
+
+
+##
+# Uses glob to find all .pd test cases in the cases/ directory.
+# Runs each of these test cases and appends their output to an output file.
+#
+def compile_all_tests():
+    counter = 0
+    for case in glob.glob("cases/*.pd"):
+        case = os.path.splitext(os.path.os.path.basename(case))[0]
+        print("\nCompiling test case: " + case)
+        with open(os.path.join("cases", case + ".expected.txt"), "w") as f:
+            output = subprocess.check_output([
+                os.path.join("..", "palladium.exe"),
+                os.path.join("cases", case + ".pd")
+            ])
+            f.write(output.decode("utf-8").replace("\r", ""))
+        counter += 1
+
+    print(f"\nSuccessfully compiled {counter} test cases")
 
 
 ##
@@ -58,12 +83,28 @@ def main(argv):
     if argv[0] == "all":
         print("Running all tests.")
         run_all_tests()
+    elif argv[0] == "compile":
+        print("Compiling all tests.")
+        compile_all_tests()
+    else:
+        case = argv[0]
+        if os.path.exists(os.path.join("cases", case + ".pd")):
+            print("Running test case: " + case)
+            run_test(case)
+        else:
+            print("Test case not found.")
+            sys.exit(1)
 
 
 if __name__ == "__main__":
     if len(sys.argv) <= 1:
         print("Palladium automated testing client.")
         print("Developed by Devin Arena.")
-        print("Usage: python3 tests.py [all|test_name]")
+        print("Usage: python3 tests.py [all|test_name|compile]")
+        print("\tall: Runs all test cases.")
+        print(
+            "\tcompile: Compiles all test cases (runs .pd files and places their output into cases/)."
+        )
+        print("\ttest_name: Runs a specific test case.")
         sys.exit(1)
     main(sys.argv[1:])
