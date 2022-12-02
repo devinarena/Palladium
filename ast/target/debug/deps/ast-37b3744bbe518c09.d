18f4b18a8803bc36247a6e60e0d933d0f3a72372
Devin Arena <devin0309@yahoo.com>
2022-12-02 15:30:05 -0500

    ast branch

---
+++ ast/target/debug/deps/ast-37b3744bbe518c09.d
@@ -0,0 +1,10 @@
+c:\Users\devin\Documents\GitHub\Palladium\ast\target\debug\deps\ast-37b3744bbe518c09.rmeta: src\main.rs src\lexer.rs src\parser.rs src\token.rs src\value.rs src\expression\expression.rs
+
+c:\Users\devin\Documents\GitHub\Palladium\ast\target\debug\deps\ast-37b3744bbe518c09.d: src\main.rs src\lexer.rs src\parser.rs src\token.rs src\value.rs src\expression\expression.rs
+
+src\main.rs:
+src\lexer.rs:
+src\parser.rs:
+src\token.rs:
+src\value.rs:
+src\expression\expression.rs:
